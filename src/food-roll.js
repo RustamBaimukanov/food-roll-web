717846dbc98cfff9c0e23dde4bb04f905d72d69b
Rustam <watashirustam@gmail.com>
2024-11-26 16:50:21 +0300

    upd icon

--- src/food-roll.js
+++ src/food-roll.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 function FoodRoll() {
     const [food, setFood] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [difficulty, setDifficulty] = useState('EASY');
 
     const fetchRandomFood = () => {
         setLoading(true);
         setError(null);
-        axios.get('http://localhost:8080/foods/roll')
+        axios.get(`http://localhost:8080/foods/roll?difficulty=${difficulty}`)
             .then(response => {
                 setFood(response.data); // Предположим, что сервер возвращает один объект еды
                 setLoading(false);
@@ -23,6 +24,16 @@ function FoodRoll() {
     return (
         <div>
             <h1>Выбор случайной еды</h1>
+            <label htmlFor="difficulty">Выберите уровень сложности: </label>
+            <select
+                id="difficulty"
+                value={difficulty}
+                onChange={(e) => setDifficulty(e.target.value)}
+            >
+                <option value="EASY">EASY</option>
+                <option value="NORMAL">NORMAL</option>
+                <option value="HARD">HARD</option>
+            </select>
             <button onClick={fetchRandomFood} disabled={loading}>
                 {loading ? 'Загрузка...' : 'Выбрать случайную еду'}
             </button>
@@ -34,7 +45,7 @@ function FoodRoll() {
                     <img
                         src={`data:image/${food.imageFileExtension};base64,${food.image}`}
                         alt={food.name}
-                        style={{ maxWidth: '100%', height: 'auto' }}
+                        style={{maxWidth: '100%', height: 'auto'}}
                     />
                 </div>
 
